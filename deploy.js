37364707cf18e5bc352fd80005c2d945526f8d8c
dreamhuan <fu_kaiqi@qq.com>
2024-11-26 13:58:21 +0800

    feat: 修改部署文件&README

--- deploy.js
+++ deploy.js
@@ -1,31 +1,40 @@
-// 来源参考： https://juejin.cn/post/6877193541844467725
-const { execSync } = require('child_process');
-const path = require('path');
+import { execSync } from "child_process";
+import { fileURLToPath } from "url";
+import path from "path";
 
 try {
+  // 解决 __dirname 和 __filename 的问题
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
   // 1. 打包
-  console.log('Building the project...');
-  execSync('npm run build', { stdio: 'inherit' });
+  console.log("Building the project...");
+  execSync("yarn build", { stdio: "inherit" });
 
   // 2. 进入目标文件夹
-  const targetDir = path.resolve(__dirname, 'digital-electronics');
+  const targetDir = path.resolve(__dirname, "digital-electronics");
   console.log(`Navigating to the target directory: ${targetDir}`);
   process.chdir(targetDir);
 
   // 3. 初始化 Git 仓库并提交所有文件
-  console.log('Initializing Git repository...');
-  execSync('git init', { stdio: 'inherit' });
-  execSync('git add -A', { stdio: 'inherit' });
-  execSync('git commit -m "deploy"', { stdio: 'inherit' });
+  console.log("Initializing Git repository...");
+  execSync("git init", { stdio: "inherit" });
+  execSync("git add -A", { stdio: "inherit" });
+  execSync('git commit -m "deploy"', { stdio: "inherit" });
 
   // 4. 推送到远程仓库的 gh-pages 分支
-  console.log('Pushing to the remote repository...');
-  execSync('git push -f git@github.com:dreamhuan/digital-electronics.git master:gh-pages', { stdio: 'inherit' });
+  console.log("Pushing to the remote repository...");
+  execSync(
+    "git push -f git@github.com:dreamhuan/digital-electronics.git master:gh-pages",
+    {
+      stdio: "inherit",
+    }
+  );
 
   // 5. 返回之前的目录
-  process.chdir('..');
-  console.log('Deployment successful!');
+  process.chdir("..");
+  console.log("Deployment successful!");
 } catch (error) {
-  console.error('Deployment failed:', error.message);
+  console.error("Deployment failed:", error.message);
   process.exit(1);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/digital-electronics/', // 设置公共路径前缀
   plugins: [react()],
   build: {
     outDir: 'digital-electronics' // 修改输出目录的名称（默认是 dist）
