efa4e6f799bb529b3b854f911449070104fe8615
dreamhuan <fu_kaiqi@qq.com>
2024-12-02 23:02:51 +0800

    feat: 异或门逻辑暴力实现

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { AndOrGate } from "./AndOrGate";
 import { NotGate } from "./NotGate";
 import Link from "./Link";
+import { In } from "./In";
 
 const BOARD_WIDTH = 1000;
 const BOARD_HEIGHT = 800;
@@ -11,14 +12,24 @@ function App() {
   const [positions, setPositions] = useState<
     Record<string, { x: number; y: number }>
   >({
-    and: { x: 50, y: 50 },
-    or: { x: 250, y: 50 },
-    not: { x: 450, y: 50 },
+    not1: { x: 150, y: 50 },
+    and1: { x: 350, y: 50 },
+    not2: { x: 150, y: 250 },
+    and2: { x: 350, y: 250 },
+    or1: { x: 550, y: 150 },
+    a: { x: 50, y: 75 },
+    b: { x: 50, y: 275 },
   }); // 初始位置
   const [linkValues, setLinkValues] = useState<Record<string, number>>({
     // 连接线的值
-    and_or: 0,
-    or_not: 0,
+    a_not1: 0,
+    a_and2: 0,
+    b_not2: 0,
+    b_and1: 0,
+    not1_and1: 0,
+    not2_and2: 0,
+    and1_or1: 0,
+    and2_or1: 0,
   });
   const [draggingId, setDraggingId] = useState(""); // 是否正在拖拽
   const [offset, setOffset] = useState({ x: 0, y: 0 }); // 鼠标偏移量
@@ -94,7 +105,6 @@ function App() {
             ></path>
           </pattern>
         </defs>
-
         <rect
           id="board"
           width={BOARD_WIDTH}
@@ -102,48 +112,141 @@ function App() {
           fill="url(#grid_item)"
         ></rect>
 
-        <AndOrGate
-          posX={positions["and"].x}
-          posY={positions["and"].y}
-          type="and"
-          onMouseDown={(e) => handleMouseDown(e, "and")}
+        <In
+          tag="A"
+          posX={positions["a"].x}
+          posY={positions["a"].y}
+          onMouseDown={(e) => handleMouseDown(e, "a")}
           setOutValue={(o: number) =>
             setLinkValues((pre) => ({
               ...pre,
-              and_or: o,
+              a_not1: o,
+              a_and2: o,
             }))
           }
         />
-        <AndOrGate
-          inX1={linkValues.and_or}
-          posX={positions["or"].x}
-          posY={positions["or"].y}
-          type="or"
-          onMouseDown={(e) => handleMouseDown(e, "or")}
+        <In
+          tag="B"
+          posX={positions["b"].x}
+          posY={positions["b"].y}
+          onMouseDown={(e) => handleMouseDown(e, "b")}
           setOutValue={(o: number) =>
             setLinkValues((pre) => ({
               ...pre,
-              or_not: o,
+              b_not2: o,
+              b_and1: o,
             }))
           }
         />
+
         <NotGate
-          inX1={linkValues.or_not}
-          posX={positions["not"].x}
-          posY={positions["not"].y}
-          onMouseDown={(e) => handleMouseDown(e, "not")}
+          inX1={linkValues.a_not1}
+          posX={positions["not1"].x}
+          posY={positions["not1"].y}
+          onMouseDown={(e) => handleMouseDown(e, "not1")}
+          setOutValue={(o: number) =>
+            setLinkValues((pre) => ({
+              ...pre,
+              not1_and1: o,
+            }))
+          }
+        />
+
+        <NotGate
+          inX1={linkValues.b_not2}
+          posX={positions["not2"].x}
+          posY={positions["not2"].y}
+          onMouseDown={(e) => handleMouseDown(e, "not2")}
+          setOutValue={(o: number) =>
+            setLinkValues((pre) => ({
+              ...pre,
+              not2_and2: o,
+            }))
+          }
+        />
+        <AndOrGate
+          inX1={linkValues.b_and1}
+          inX2={linkValues.not1_and1}
+          posX={positions["and1"].x}
+          posY={positions["and1"].y}
+          type="and"
+          onMouseDown={(e) => handleMouseDown(e, "and1")}
+          setOutValue={(o: number) =>
+            setLinkValues((pre) => ({
+              ...pre,
+              and1_or1: o,
+            }))
+          }
+        />
+        <AndOrGate
+          inX1={linkValues.a_and2}
+          inX2={linkValues.not2_and2}
+          posX={positions["and2"].x}
+          posY={positions["and2"].y}
+          type="and"
+          onMouseDown={(e) => handleMouseDown(e, "and2")}
+          setOutValue={(o: number) =>
+            setLinkValues((pre) => ({
+              ...pre,
+              and2_or1: o,
+            }))
+          }
+        />
+        <AndOrGate
+          inX1={linkValues.and1_or1}
+          inX2={linkValues.and2_or1}
+          posX={positions["or1"].x}
+          posY={positions["or1"].y}
+          type="or"
+          onMouseDown={(e) => handleMouseDown(e, "or1")}
         />
         <Link
-          posInX={positions["and"].x + 132}
-          posInY={positions["and"].y + 25}
-          posOutX={positions["or"].x - 32}
-          posOutY={positions["or"].y + 10}
+          posInX={positions["a"].x + 25}
+          posInY={positions["a"].y}
+          posOutX={positions["not1"].x - 35}
+          posOutY={positions["not1"].y + 25}
         />
         <Link
-          posInX={positions["or"].x + 132}
-          posInY={positions["or"].y + 25}
-          posOutX={positions["not"].x - 32}
-          posOutY={positions["not"].y + 25}
+          posInX={positions["a"].x + 25}
+          posInY={positions["a"].y}
+          posOutX={positions["and2"].x - 35}
+          posOutY={positions["and2"].y + 5}
+        />
+        <Link
+          posInX={positions["b"].x + 25}
+          posInY={positions["b"].y}
+          posOutX={positions["not2"].x - 35}
+          posOutY={positions["not2"].y + 25}
+        />
+        <Link
+          posInX={positions["b"].x + 25}
+          posInY={positions["b"].y}
+          posOutX={positions["and1"].x - 35}
+          posOutY={positions["and1"].y + 10}
+        />
+        <Link
+          posInX={positions["not1"].x + 132}
+          posInY={positions["not1"].y + 25}
+          posOutX={positions["and1"].x - 32}
+          posOutY={positions["and1"].y + 40}
+        />
+        <Link
+          posInX={positions["not2"].x + 132}
+          posInY={positions["not2"].y + 25}
+          posOutX={positions["and2"].x - 32}
+          posOutY={positions["and2"].y + 40}
+        />
+        <Link
+          posInX={positions["and1"].x + 132}
+          posInY={positions["and1"].y + 25}
+          posOutX={positions["or1"].x - 32}
+          posOutY={positions["or1"].y + 5}
+        />
+        <Link
+          posInX={positions["and2"].x + 132}
+          posInY={positions["and2"].y + 25}
+          posOutX={positions["or1"].x - 32}
+          posOutY={positions["or1"].y + 45}
         />
       </svg>
     </div>
---
+++ src/In.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+
+export const In = ({
+  inX1,
+  posX,
+  posY,
+  tag,
+  onMouseDown,
+  setOutValue,
+}: {
+  inX1?: number;
+  posX: number;
+  posY: number;
+  tag?: string;
+  onMouseDown: (e: React.MouseEvent) => void;
+  setOutValue?: (out: number) => void;
+}) => {
+  const [in1, setIn1] = useState(0);
+  const [out, setOut] = useState(0);
+
+  useEffect(() => {
+    if (inX1 !== undefined) {
+      setIn1(inX1);
+    }
+  }, [inX1]);
+
+  useEffect(() => {
+    const o = in1;
+    setOut(o);
+    setOutValue?.(o);
+  }, [in1]);
+
+  return (
+    <>
+      <g transform={`translate(${posX}, ${posY})`}>
+        <text
+          x="0"
+          y="0"
+          fontSize="16"
+          textAnchor="middle"
+          dominantBaseline="middle"
+          fill="black"
+          style={{ cursor: "pointer", userSelect: "none" }}
+          onClick={() => setIn1(in1 === 0 ? 1 : 0)}
+        >
+          {tag}:
+        </text>
+        <text
+          x="15"
+          y="0"
+          fontSize="16"
+          textAnchor="middle"
+          dominantBaseline="middle"
+          fill="black"
+          style={{ cursor: "pointer", userSelect: "none" }}
+          onClick={() => setIn1(in1 === 0 ? 1 : 0)}
+        >
+          {in1}
+        </text>
+      </g>
+      <rect
+        x={posX - 10}
+        y={posY - 10}
+        width="20"
+        height="20"
+        fill="transparent"
+        onMouseDown={onMouseDown}
+        style={{ cursor: "move" }}
+      />
+    </>
+  );
+};
